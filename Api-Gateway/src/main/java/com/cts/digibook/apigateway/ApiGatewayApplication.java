645e2cc47ccc5c1152831c13348a02848915af4c
Karthik Bharadwaj <74718329+karthikbharadwa-j95@users.noreply.github.com>
2022-10-17 11:52:46 +0530

    Add files via upload

--- Api-Gateway/src/main/java/com/cts/digibook/apigateway/ApiGatewayApplication.java
+++ Api-Gateway/src/main/java/com/cts/digibook/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,15 @@
 package com.cts.digibook.apigateway;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 //@EnableEurekaClient
@@ -11,5 +18,24 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	
+	
+	@Bean 
+    public CorsWebFilter corsWebFilter() { 
+ 
+ 
+        final CorsConfiguration corsConfig = new CorsConfiguration(); 
+        corsConfig.setAllowedOrigins(Collections.singletonList("*")); 
+        corsConfig.setMaxAge(3600L); 
+        corsConfig.setAllowedMethods(Arrays.asList("*")); 
+        corsConfig.addAllowedHeader("*"); 
+ 
+ 
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); 
+        source.registerCorsConfiguration("/**", corsConfig); 
+ 
+ 
+        return new CorsWebFilter(source); 
+    } 
 
 }
